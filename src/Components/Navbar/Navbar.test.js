f8ba0d29edc440dbcba024cbf333b8a26032cf77
julie-desvaux <desvaux.julie@gmail.com>
2021-11-15 17:26:29 +0100

    fix test navbar

--- src/Components/Navbar/Navbar.test.js
+++ src/Components/Navbar/Navbar.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 // COMPONENT
 import Navbar from "./Navabar";
@@ -10,29 +9,29 @@ it("should display links", () => {
 	const { getByText } = render(<Navbar />);
 	const linkMoi = getByText(/moi/i);
 	expect(linkMoi).toBeInTheDocument();
-	expect(screen.getByText(/moi/i).closest("a")).toHaveAttribute("href", "#me");
+	expect(linkMoi.closest("a")).toHaveAttribute("href", "#me");
 
 	const linkCompetences = getByText(/compétences/i);
 	expect(linkCompetences).toBeInTheDocument();
-	expect(screen.getByText(/compétences/i).closest("a")).toHaveAttribute("href", "#competences");
+	expect(linkCompetences.closest("a")).toHaveAttribute("href", "#competences");
 
 	const linkXP = getByText(/expériences/i);
 	expect(linkXP).toBeInTheDocument();
-	expect(screen.getByText(/expériences/i).closest("a")).toHaveAttribute("href", "#xp");
+	expect(linkXP.closest("a")).toHaveAttribute("href", "#xp");
 
 	const linkPortfolio = getByText(/portfolio/i);
 	expect(linkPortfolio).toBeInTheDocument();
-	expect(screen.getByText(/portfolio/i).closest("a")).toHaveAttribute("href", "#portfolio");
+	expect(linkPortfolio.closest("a")).toHaveAttribute("href", "#portfolio");
 
 	const linkEducation = getByText(/education/i);
 	expect(linkEducation).toBeInTheDocument();
-	expect(screen.getByText(/education/i).closest("a")).toHaveAttribute("href", "#education");
+	expect(linkEducation.closest("a")).toHaveAttribute("href", "#education");
 
 	const linkMooc = getByText(/mooc/i);
 	expect(linkMooc).toBeInTheDocument();
-	expect(screen.getByText(/mooc/i).closest("a")).toHaveAttribute("href", "#mooc");
+	expect(linkMooc.closest("a")).toHaveAttribute("href", "#mooc");
 
 	const linkbenevolat = getByText(/bénévolat/i);
 	expect(linkbenevolat).toBeInTheDocument();
-	expect(screen.getByText(/bénévolat/i).closest("a")).toHaveAttribute("href", "#benevolat");
+	expect(linkbenevolat.closest("a")).toHaveAttribute("href", "#benevolat");
 });
