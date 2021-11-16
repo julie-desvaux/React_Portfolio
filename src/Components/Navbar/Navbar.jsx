ef19815209945116600ab645e1c9a3b1ef8a22f2
julie-desvaux <desvaux.julie@gmail.com>
2021-11-16 16:20:20 +0100

    [Navbar]: fix css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
 			{({ open }) => (
 				<>
 					<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-						<div className="relative flex items-center justify-between h-16">
+						<div className="relative flex items-center justify-between h-12">
 							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 								{/* Mobile menu button*/}
 								<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -57,7 +57,7 @@ export default function Navbar() {
 													item.current
 														? "bg-white text-blue font-bold"
 														: "text-gray-200 hover:bg-navyBlue hover:text-white font-medium",
-													"px-3 py-2 rounded-md text-sm font-medium uppercase tracking-widest"
+													"px-3 py-2 rounded-md text-sm font-medium uppercase tracking-wide"
 												)}
 												aria-current={item.current ? "page" : undefined}
 											>
